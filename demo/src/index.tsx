e4be2770eb81c058e1a7b81b02aaaa0df3c7598a
AshleyZG <gsarazh@gmail.com>
2024-02-01 01:26:07 -0500

    input field

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
+
+function RootComponent(): JSX.Element {
+
+  // TODO: store data
+	const [data, setdata] = useState<any>([]);
+  const [criteriaInput, setCriteriaInput] = useState<string|undefined>(undefined);
+
+
+  const handleSubmit = () => {
+    // TODO
+    console.log('TODO: handleSubmit');
+  }
+
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try{
+				// TODO: fetch data online
+
+			} catch (error){
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<React.StrictMode>
+        <div id="classifier-panel">
+                <label htmlFor="criteria">Specify a criteria:</label>
+                <input type="text" id="criteria" ref={(input) => setCriteriaInput(input?.value)} />
+                <button onClick={handleSubmit}>Submit</button>
+        </div>
+		</React.StrictMode>
+	);
+}
+
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+	document.getElementById('root') as HTMLElement
+  );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<RootComponent />
 );
 
 // If you want to start measuring performance in your app, pass a function

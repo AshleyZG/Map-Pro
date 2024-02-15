92aa7c36361a3f777fa55b4fd9e9bc7918d2c8b3
AshleyZG <gsarazh@gmail.com>
2024-02-15 15:23:50 -0500

    add: flow data from segment embeddings

--- demo/src/utils.ts
+++ demo/src/utils.ts
@@ -29,3 +29,32 @@ export function generate_flow_data (data: any): SankeyData{
     return results;
     
 }
+
+
+export function generate_flow_data_from_segs(data: any): SankeyData{
+    var nodeIDMap: {[key: string]: number} = {};
+    var linkIDMap: {[key: string]: number} = {};
+    var results: SankeyData = {nodes: [], links: []};
+    data.forEach((items: any) => {
+        items.segLabels.forEach((item: any, idx: number) => {
+            // update nodes first
+            if (!(`N${item}L${idx}` in nodeIDMap)){
+                nodeIDMap[`N${(item)}L${idx}`] = results.nodes.length;
+                results.nodes.push({name: `N${item}L${idx}`});
+            }
+            
+            // if not the first level, update links as well
+            if (idx > 0){
+                var source = nodeIDMap[`N${items.segLabels[idx-1]}L${idx-1}`];
+                var target = nodeIDMap[`N${(item)}L${idx}`];
+
+                if (!(`S${source}T${target}L${idx}` in linkIDMap)){
+                    linkIDMap[`S${source}T${target}L${idx}`] = results.links.length;
+                    results.links.push({source: source, target: target, value: 0});
+                }
+                results.links[linkIDMap[`S${source}T${target}L${idx}`]].value += 1;
+            }
+        });
+    })
+    return results;
+}
